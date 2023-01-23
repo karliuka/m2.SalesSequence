a9cb26882b80cb1a95942f67a4f466bff46e0412
Vitalii Karliuka <karliuka.vitalii@gmail.com>
2023-01-23 23:22:58 +0200

    fixed code style

--- Setup/Uninstall.php
+++ Setup/Uninstall.php
@@ -10,6 +10,7 @@ namespace Faonni\SalesSequence\Setup;
 use Magento\Framework\Setup\UninstallInterface;
 use Magento\Framework\Setup\ModuleContextInterface;
 use Magento\Framework\Setup\SchemaSetupInterface;
+use Faonni\SalesSequence\Api\Data\ProfileInterface;
 
 /**
  * Uninstall sales sequence
@@ -23,24 +24,15 @@ class Uninstall implements UninstallInterface
      * @param ModuleContextInterface $context
      * @return void
      */
-    public function uninstall(SchemaSetupInterface $setup, ModuleContextInterface $context)
-    {
+    public function uninstall(
+        SchemaSetupInterface $setup,
+        ModuleContextInterface $context
+    ) {
         $setup->startSetup();
-        $this->removeColumns($setup);
-        $setup->endSetup();
-    }
-
-    /**
-     * Remove columns
-     *
-     * @param SchemaSetupInterface $setup
-     * @return void
-     */
-    protected function removeColumns(SchemaSetupInterface $setup)
-    {
         $setup->getConnection()->dropColumn(
             $setup->getTable('sales_sequence_profile'),
-            'pattern'
+            ProfileInterface::PATTERN
         );
+        $setup->endSetup();
     }
 }
